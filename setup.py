33744f448a20cd7dfa0259aa6341dce2baacc663
Sam Stavinoha <smlstvnh@gmail.com>
2014-03-29 13:18:17 -0500

    pip is passing --install-option too far

--- setup.py
+++ setup.py
@@ -1,19 +1,26 @@
-from setuptools import setup, find_packages
+import argparse
+import os
+import sys
 
+from setuptools import setup, find_packages
 from setuptools.command.develop import develop as develop_command
+#from setuptools.command.install import install as install_command
+
+from pbr.packaging import parse_requirements
 from pbr.packaging import LocalInstall as install_command
 
-import ipdb
+MINIMAL = ('Perform the minimal, "localhost discovery" only Satori '
+           'installation. i.e exclude heavy dependencies: '
+           'novaclient, paramiko, etc.')
 
-
-def add_option(cls, dag, description, boolean=False):
+def add_option(cls, tag, description, boolean=False):
     """Return tuple of user_options, boolean_options."""
     cls.user_options = [
-        (dag, None, description)
+        (tag, None, description)
     ] + cls.user_options
 
     if boolean:
-        cls.boolean_options = [dag] + cls.boolean_options
+        cls.boolean_options = [tag] + cls.boolean_options
 
     # no dupes
     cls.user_options = list(set(cls.user_options))
@@ -25,20 +32,21 @@ class CustomDevelopCommand(develop_command):
 
     command_name = 'develop'
     user_options, boolean_options = add_option(
-        develop_command, 'minimal',
-        'Some option to indiciate a minimal install',
-        boolean=True)
+        develop_command, 'minimal', MINIMAL, boolean=True)
 
     def __init__(self, *args, **kwargs):
-        print "MODIFIED INIT: %s" % self.__class__
-        self.minimal = False
+        self.minimal = None
         develop_command.__init__(self, *args, **kwargs)
 
     def run(self, *args, **kwargs):
-        if self.minimal:
-            print "SELF.MINIMAL"
         print "MODIFIED RUN: %s" % self.__class__
-        print self.distribution.install_requires
+        if self.minimal:
+            self.distribution.install_requires = (
+                parse_requirements(
+                    ['minimal-requirements.txt']))
+            print "SELF.MINIMAL"
+        print "install requires: %s" % (
+                self.distribution.install_requires)
         develop_command.run(self, *args, **kwargs)
 
 
@@ -46,23 +54,50 @@ class CustomInstallCommand(install_command):
 
     command_name = 'install'
     user_options, boolean_options = add_option(
-        install_command, 'minimal',
-        'Some option to indiciate a minimal install',
-        boolean=True)
+        install_command, 'minimal', MINIMAL, boolean=True)
 
     def __init__(self, *args, **kwargs):
-        print "MODIFIED INIT: %s" % self.__class__
-        self.minimal = False
+        self.minimal = None
         install_command.__init__(self, *args, **kwargs)
 
     def run(self, *args, **kwargs):
-        if self.minimal:
-            print "SELF.MINIMAL"
         print "MODIFIED RUN: %s" % self.__class__
-        print self.distribution.install_requires
+        if self.minimal:
+            self.distribution.install_requires = (
+                parse_requirements(
+                    ['minimal-requirements.txt']))
+            print "SELF.MINIMAL"
+        print "install requires: %s" % (
+                self.distribution.install_requires)
         install_command.run(self, *args, **kwargs)
 
+
+"""
+def parse():
+    if '--help' not in sys.argv:
+        parser = argparse.ArgumentParser(description="Satori installation options.")
+        parser.add_argument("--minimal", action="store_true", help=MINIMAL)
+        config, unknown = parser.parse_known_args()
+        if config.minimal:
+
+
+    if '--minimal' in sys.argv:
+            print "SETTING PBR_REQUIREMENTS_FILES"
+            os.environ['PBR_REQUIREMENTS_FILES'] = 'minimal-requirements.txt'
+            sys.argv.remove('--minimal')
+"""
+
 if __name__ == '__main__':
+
+    if '--help' not in sys.argv:
+        #parser = argparse.ArgumentParser(description="Satori installation options.")
+        #parser.add_argument("--minimal", action="store_true", help=MINIMAL)
+        #config, unknown = parser.parse_known_args()
+        #if config.minimal:
+        if '--minimal' in sys.argv:
+            print "SETTING PBR_REQUIREMENTS_FILES"
+            os.environ['PBR_REQUIREMENTS_FILES'] = 'minimal-requirements.txt'
+            sys.argv.remove('--minimal')
 
     setup(
         setup_requires=['pbr'],
