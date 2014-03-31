63d20ede699f5cd5ab5fd8b653b5c213db798023
Sam Stavinoha <smlstvnh@gmail.com>
2014-03-31 11:19:27 -0500

    dont write full reqs on `egg_info`

--- setup.py
+++ setup.py
@@ -8,15 +8,18 @@ from setuptools import setup
 
 if __name__ == '__main__':
 
-    if '--help' not in sys.argv:
+    if not any(k in sys.argv[1:] for k in ('--help', '--help-commands')):
         #parser = argparse.ArgumentParser(description="Satori installation options.")
         #parser.add_argument("--minimal", action="store_true", help=MINIMAL)
         #config, unknown = parser.parse_known_args()
         #if config.minimal:
-        if '--minimal' in sys.argv:
+        if '--minimal' in sys.argv or sys.argv[1] not in ('install', 'develop'):
             print "SETTING PBR_REQUIREMENTS_FILES"
             os.environ['PBR_REQUIREMENTS_FILES'] = 'minimal-requirements.txt'
-            sys.argv.remove('--minimal')
+            try:
+                sys.argv.remove('--minimal')
+            except ValueError:
+                pass
 
     setup(
         setup_requires=['pbr-samstav'],
